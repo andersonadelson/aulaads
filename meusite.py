96da440eeba8a118263d569735250aa8c6a545be
andersonadelson <anderson_adelson@yahoo.com.br>
2023-02-05 04:45:23 +0000

    novas rotas

--- meusite.py
+++ meusite.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     return render_template("index.html")
+    
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
